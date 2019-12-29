36b16e68430afaed428157918a97f5523bde243a
Kristján Valur Jónsson <sweskman@gmail.com>
2019-12-29 18:03:41 +0000

    create initial tests

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,143 @@
 #include "stackman.h"
 
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* simple test ithout modifying stack pointers.
+ * test that the callback is called wiht valid
+ * stackpoiners and the stage member in the 
+ * correct order.
+ */
+typedef struct ctxt01
+{
+	void *sp[2];
+} ctxt01;
+
+/* test retrieval of stack pointer */
+void *test_01_cb(void* context, void *sp, int stage)
+{
+	ctxt01 *c = (ctxt01*)context;
+	assert(stage == 0 || stage == 1);
+
+	/* check that stages are init in order */
+	assert(!c->sp[1]);
+	if (stage)
+		assert(c->sp[0]);
+	else
+		assert(!c->sp[1]);
+	c->sp[stage] = sp;
+	if (stage == 0)
+		return sp ;/* no stack switching */
+	return (void*)1; /* test return argument */
+}
+void test_01(void)
+{
+	/* since we are not switching stacks, it is ok to keep context on stack */
+	ctxt01 c;
+	void *sp;
+	memset(&c, 0, sizeof(c));
+
+	sp = stackman_switch(&test_01_cb, &c);
+
+	assert(sp == (void*)1);
+	assert(c.sp[0] == c.sp[1]);
+	assert(STACKMAN_SP_LE(c.sp[0], (void*)&c));
+}
+
+
+/* a more complex test, performing a long jump out of a subroutine
+ * the context now stores a stub that can be jumped to 
+ */
+typedef struct ctxt02
+{
+	void *stack_far, *stack_near;
+	void *buf;
+	size_t size;
+	/* every call to cb logs val in log and increments */
+	int val;
+	int log[10];
+	int counter;
+} ctxt02;
+
+/* helpers to save and restore stack contents to a buffer */
+void save_stack(void *sp, void *buf, size_t size)
+{
+	if (STACKMAN_DIRECTION == 0)
+		memcpy(buf, sp, size);
+	else
+		memcpy(buf, (void*)((char*)sp-size), size);
+}
+
+void restore_stack(void *sp, void *buf, size_t size)
+{
+	if (STACKMAN_DIRECTION == 0)
+		memcpy(sp, buf, size);
+	else
+		memcpy((void*)((char*)sp-size), buf, size);
+}
+
+void *test_02_cb(void* context, void *sp, int stage)
+{
+	ctxt02 *c = (ctxt02*)context;
+	c->log[c->counter++] = c->val;
+	if (c->val == 0) {
+		/* storing stub */
+		if (stage == 0) {
+			c->stack_near = sp;
+			c->size = STACKMAN_SP_SUB((char*)c->stack_far, (char*)sp);
+			c->buf = malloc(c->size);
+			save_stack(sp, c->buf, c->size);
+		}
+		return sp;
+	}
+	/* now we are jumping.  no need to save old stack */
+	if (stage == 0) {
+		return c->stack_near;
+	} else {
+		restore_stack(c->stack_near, c->buf, c->size);
+		return sp;
+	}
+}
+ 
+void test_02_called(ctxt02 *c)
+{
+	/* right, perform the jump to the original stack pointer */
+	c->val = 1;
+	stackman_switch(&test_02_cb, c);
+}
+void test_02(void)
+{
+	int stack_marker;
+	static ctxt02 c;  /* keep away from stack */
+	memset(&c, 0, sizeof(c));
+	c.stack_far = (void*)&stack_marker;
+
+	/* first time around, get the stack pointer */
+	stackman_switch(&test_02_cb, &c);
+
+	if (c.val == 0) {
+		assert(c.counter == 2);
+		/* first time.  We now have the sp to return to the original one
+		 * lets long jump out of a recursive function here
+		 */
+		assert(c.buf);
+		assert(c.size);
+		test_02_called(&c);
+		assert(0); /* never reach this */
+	}
+	assert(c.val == 1);
+	assert(c.counter == 4);
+}
+
 int main(int argc, char*argv[])
 {
+	test_01();
+	printf("test_01 ok\n");
+	test_02();
+	printf("test_02 ok\n");
 	return 0;
 }
