0f50c0962c2faf864edb6edaf50589492fcb8a01
Kristján Valur Jónsson <sweskman@gmail.com>
2020-11-29 13:05:12 +0000

    add define when building library to disable call indirection

--- src/stackman.c
+++ src/stackman.c
@@ -1,5 +1,7 @@
 /* 
+ * Stackman library.
  * Implementation of the stack switching function, if it is done
  * in C (using in-line assembly)
  */
+#define STACKMAN_BUILD_LIB
 #include "stackman_impl.h"
