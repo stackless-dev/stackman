2469fbe56ec23ad3e49fdaa675617376082be91f
Kristján Valur Jónsson <sweskman@gmail.com>
2020-12-03 16:46:10 +0000

    Preserve ebp in stackman_switch, win_x86. Do not compile runtime checks in c-wrapper.

--- src/platforms/switch_x86_msvc.asm
+++ src/platforms/switch_x86_msvc.asm
@@ -5,13 +5,12 @@
 .code
 
 stackman_switch_raw PROC callback:DWORD, context:DWORD
-  
+  ;Assembler has already pushed the basepointer and saved it.
   ;save registers. EAX ECX and EDX are available for function use and thus
   ;do not have to be stored.
   push ebx
   push esi
   push edi
-  push ebp
   
   mov esi, callback ;      /* save 'callback' for later */
   mov edi, context ;       /* save 'context' for later         */
@@ -22,8 +21,13 @@ stackman_switch_raw PROC callback:DWORD, context:DWORD
   push 0;                  /* arg 2: opcode STACKMAN_OP_SAVE     */ 
   push edi ;               /* arg 1: context                     */
   call esi ;               /* call callback()                    */
-
-  mov esp, eax;            /* change the stack pointer */
+  add esp, 12;
+  
+  mov ecx, eax;			   /* change stack pointer to eax, preserving */
+  sub ecx, esp;            /* base pointer offset from esp */
+  mov esp, eax;
+  add ebp, ecx;
+  
 
   push eax ;               /* arg 3: current (new) stack pointer */
   push 1;                  /* arg 2: opcode STACKMAN_OP_RESTORE  */ 
@@ -31,7 +35,6 @@ stackman_switch_raw PROC callback:DWORD, context:DWORD
   call esi ;               /* call callback()                    */
   add esp, 12
 
-  pop  ebp
   pop  edi
   pop  esi
   pop  ebx
