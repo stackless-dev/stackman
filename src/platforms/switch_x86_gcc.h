27bf645bbe63910de3a28b139923aefef407b49d
Kristján Valur Jónsson <sweskman@gmail.com>
2020-12-04 21:05:32 +0000

    sysv_386: make stack aligned in stackman_call Incoming stack pointer must be adjusted to make this happen. Also adjust old stack pointer to make it correspond to incoming stack pointer.

--- src/platforms/switch_x86_gcc.h
+++ src/platforms/switch_x86_gcc.h
@@ -106,10 +106,13 @@ void *stackman_call(stackman_cb_t callback, void *context, void *stack_pointer)
   
   /* sp = store stack pointer in ebx */
   __asm__ ("movl %%esp, %%ebx" : : : "ebx");
-  __asm__ ("movl %%esp, %[sp]" : [sp] "=r" (old_sp));
+  /* save old stack pointer at same offset as new stack pointer */
+  __asm__ ("leal 4(%%esp), %[sp]" : [sp] "=r" (old_sp));
   
   /* set stack pointer from provided using assembly */
   __asm__ ("movl %[sp], %%esp" :: [sp] "r" (stack_pointer));
+  /* subtract 4 bytes to make it 16 byte alighed after pushing 3 args */
+  __asm__ ("subl $4, %esp");
 
   result = callback(context, STACKMAN_OP_CALL, old_sp);
   /* restore stack pointer */
