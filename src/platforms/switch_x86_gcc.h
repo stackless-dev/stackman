f80c7448e44d572e1cc83eb12be6bdb240280895
Kristján Valur Jónsson <sweskman@gmail.com>
2020-12-04 09:04:50 +0000

    Update x86_gcc. send 16 bit aligned stack to callbacks. Don´t include callback arguments in callback stack. Update base pointe when switching stack.

--- src/platforms/switch_x86_gcc.h
+++ src/platforms/switch_x86_gcc.h
@@ -52,31 +52,41 @@ void *result;
       /* adjust stack pointer to be 16 byte (4 quad) aligned with 
        * room for call args
        * since the call instruction, 5 quads have
-       * been pushed (ip, bp, bx, si, di), need extra 3 quads
+       * been pushed (ip, bp, bx, si, di), need extra 3 quads (0xa)
        * for alignment, which fits our three quad call args.
+       * Add another 16 (0x10) bytes, so that we can pass to the callbacks
+       * a 16 byte boundary that lies above the pushed arguments
+       * so that the call arguments of the functions aren't clobbered
+       * by memory transfer.
        */
-    "subl $12, %%esp\n"
+    "subl $0x1a, %%esp\n"
     "movl %[cb], %%esi\n"  /* save 'callback' for later */
     "movl %[ctx], %%edi\n" /* save 'context' for later         */
 
-    /* first call */     
-    "movl %%esp, 8(%%esp)\n"  /* arg 2 sp */
+    /* first call */   
+    "leal 0x10(%%esp), %%ebx\n" /* arg 2 adjusted sp (also in nv. reg) */
+    "movl %%ebx, 8(%%esp)\n"
     "movl $0, 4(%%esp)\n"     /* arg 1 opcode STACKMAN_OP_SAVE */
     "movl %%edi, 0(%%esp)\n"  /* arg 0 context */
     "call *%%esi\n"
 
-    /* restore esp */
-    "movl %%eax, %%esp\n"
+    /* restore esp, re-adding shadow space */
+    "leal -0x10(%%eax), %%esp\n"
+    
+    /* and adjust ebp with difference between new and old */
+    "subl %%ebx, %%eax\n"
+    "addl %%eax, %%ebp\n"
      
     /* second call */
-    "movl %%eax, 8(%%esp)\n"  /* arg 2 sp */
+    "leal 0x10(%%esp), %%eax\n"  /* arg 2 sp */
+    "movl %%eax, 8(%%esp)\n"
     "movl $1, 4(%%esp)\n"     /* arg 1 opcode STACKMAN_OP_RESTORE */
     "movl %%edi, 0(%%esp)\n"  /* arg 0 context */
     "call *%%esi\n"
 
     "movl %%eax, %[result]\n"
 
-    "addl $12, %%esp\n"
+    "addl $0x1a, %%esp\n"
     : [result] "=r" (result)              /* output variables */
     : [cb] "r" (callback),       /* input variables  */
       [ctx] "r" (context)
