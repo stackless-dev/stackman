ac24664e3f7e66779c19b72601d001c9ab9d1387
Kristján Valur Jónsson <sweskman@gmail.com>
2020-12-06 21:08:03 +0000

    remove redundant winodows build scripts

--- vs2017/build_x64.cmd
+++
@@ -1,3 +0,0 @@
-rem Build x64 with visualstudio2017buildtools
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
-msbuild stackman.sln /p:Platform=x64
--- vs2017/build_x86.cmd
+++
@@ -1,3 +0,0 @@
-rem Build x86 with visualstudio2017buildtools
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
-msbuild stackman.sln /p:Platform=x86
