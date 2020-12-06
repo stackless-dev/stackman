a55166cd288354db7a14cca8774b8f1727ea460b
Kristján Valur Jónsson <sweskman@gmail.com>
2020-12-06 13:53:04 +0000

    Quote the paths in the .cmd files

--- vs2019/build_x64.cmd
+++ vs2019/build_x64.cmd
@@ -1,3 +1,3 @@
 rem Build x64 with visualstudio2017buildtools
-C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 msbuild stackman.sln /p:Platform=x64
