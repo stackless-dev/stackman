71e02069e3212b24f64e52008a4c88f5154a4280
Kristján Valur Jónsson <sweskman@gmail.com>
2021-04-11 17:43:08 +0000

    add zero-stack arg support for linux x86

--- stackman/platforms/gen_asm.c
+++ stackman/platforms/gen_asm.c
@@ -2,7 +2,7 @@
  * of assembly code under e.g. Gcc.
  * use by calling, for example:
  *  cc -S -m32 -fcf-protection=none gen_asm.c
- * and examinine the generated test.s assembly code.
+ * and examinine the generated gen_asm.s assembly code.
  * -m32 selects 32 bit mode, use other directives to select a different platform.
  * The -fcf-protection flag disables generation of intel CET compatible code, but stack switching
  * is not compatible with the proposed shadow stack.  Only the latest compilers have it.
